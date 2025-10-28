dfe29c92f8b5b70c95a4cd2eff253e6b70c52233
RuiloAbyss <ruilo.abisai@outlook.com>
2025-10-27 19:46:35 -0700

    DEBUG Registro y login

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,7 +5,7 @@ let users = [
     {//REMOVER CUANDO SE IMPLEMENTE EN FIREBASE
         id: '@sa_j5l0bY8bXUuJ8Fh',
         name: 'sa',
-        password: '$2a$10$7qJ6r0vH6H9j5l0bY8bXUuJ8Fh8bXUuJ8Fh8bXUuJ8Fh8bXUuJ8Fh8bXUu', //hashed 'admin123'
+        password: '$2b$10$4Q/deWF2WqA1BBh6QInNmuR4foPb5tk6p2xjbotd6fc9au4H92Cba', //hashed 'admin123'
         email: 'systemadmon@store.com',
         role: 'admin',
         address: 'Av. Tecnologico 777'
@@ -27,45 +27,6 @@ function getAllUsers() {// DEBUG (BORRAR DESPUES)
     }));
 }
 
-async function createUser({email, password, name, address}) {
-    const exiting = users.find((u) => u.email === email)
-    if(exiting) return null;
-    const hashedPass = await bcrypt.hashSync(password, 10); //await hace que la función espere a complir la sentencia que engloba
-    const user = {
-        id: randomUUID(),
-        email:email,
-        password:hashedPass,
-        name:name,
-        role:'user',
-        address:address
-    };
-    users.push(user);
-    return{ id:user.id, email: user.email, name:user.name};
-}
-
-async function editUser(id, {email, password, name, address}){
-    const user = users.find((u) => u.id === id);
-    if(!user) return null;
-    user.email = email || user.email;
-    user.password = password ? await bcrypt.hashSync(password, 10) : user.password;
-    user.name = name || user.name;
-    user.address = address || user.address;
-    return { id:user.id, email: user.email, name:user.name};
-}
-
-async function deleteUser(id){
-    const index = users.findIndex((u) => u.id === id);
-    if(index === -1) return null;
-    users.splice(index, 1);
-    return true;
-}
-
-module.exports = { findById, getAllUsers, createUser, editUser, deleteUser};
-
-function findById(id){
-    return users.find((u) => u.id === id) || null;
-}
-
 function findByEmail(email){
     return users.find((u) =>  u.email === email) || null;
 }
@@ -83,7 +44,7 @@ async function createUser({email, password, name, address}) {
         address:address
     };
     users.push(user);
-    return{ id:user.id, email: user.email, name:user.name};
+    return{ id:user.id, email: user.email, name:user.name, password:user.password }; //QUITAR PASSWIRD AL FINAL
 }
 
 async function editUser(id, {email, password, name, address}){
@@ -103,4 +64,4 @@ async function deleteUser(id){
     return true;
 }
 
-module.exports = { findById, findByEmail, createUser, editUser, deleteUser };
+module.exports = { findById, getAllUsers, findByEmail, createUser, editUser, deleteUser };
